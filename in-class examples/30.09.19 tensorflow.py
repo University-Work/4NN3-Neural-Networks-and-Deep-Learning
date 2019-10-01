d9a647a98364b0ff136301dae4b7923acca9d215
Yury Stanev <yury.stanev@menlolab.com>
2019-10-01 15:07:23 -0400

    some comments

--- in-class examples/30.09.19 tensorflow.py
+++ in-class examples/30.09.19 tensorflow.py
@@ -1,8 +1,10 @@
+# Have to Use tensorflow 1.14 b/c version 2.0 has some issues
 import tensorflow as tf
 
 b = tf.Variable(tf.zeros((100,)))
-W = tf.Variable(tf.random_uniform((784, 100), -1, 1))
-x = tf.placeholder(tf.float32, (1, 784))
+W = tf.Variable(tf.random.uniform((784, 100), -1, 1))
+x = tf.compat.v1.placeholder(tf.float32, (1, 784))
 
 # h = ReLU(Wx+b)
 h = tf.nn.relu(tf.matmul(x, W) + b)
+print(h)
