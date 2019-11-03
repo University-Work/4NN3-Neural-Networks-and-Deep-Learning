9f00c8c8252f138fd49d781935ba0e4eb75f5364
Yury Stanev <yury.stanev@menlolab.com>
2019-11-03 18:55:03 -0500

    Add more comments

--- midterm_practice/perceptron_practic.py
+++ midterm_practice/perceptron_practic.py
@@ -8,21 +8,28 @@ import numpy as np
 import random
 import matplotlib.pyplot as plt
 
-data = np.loadtxt('magic04_space_delimited.txt', delimiter=' ')
+data = np.loadtxt('magic04_space_delimited.txt',
+                  delimiter=' ')  # Load DATA into matrix
 
+# Pick a 1000 points for training
 #    ROW      COLUMN
 # [ from:to, from:to ]
-X = data[11832:12832, 0:9]
-y = data[11832:12832, 10]
+X = data[11832:12832, 0:9]  # attributes
+y = data[11832:12832, 10]  # labels
+
+# Augmented Data Matrix
+# Y-axis = 0, X-axis = 1
+# * Add a column of 1 to the beggining of the matrix for BIAS
+X = np.concatenate((np.ones((1000, 1)), X), axis=1)
 
 
 def perceptron_sgd_plot(X, y):
     '''
     Train Perceptron & Plot the Total Loss in each Epoch.
 
-    :param X: data samples
-    :param y: data labels
-    :return: weight vector as a numpy array
+        - param X: data samples
+        - param y: data labels
+        - return: weight vector as a numpy array
     '''
 
     # Initialize the weight vector for the perceptron with zeros
@@ -37,16 +44,18 @@ def perceptron_sgd_plot(X, y):
             if(np.dot(X[i], w)*y[i]) <= 0:  # Misclassification condition yi⟨xi,w⟩ ≤ 0
                 total_error += (np.dot(X[i], w)*y[i])  # Calculate the Error
                 # Update rule for the weights w = w + yi ∗ xi
-                w = w = eta*X[i]*y[i]
+                w = eta*X[i]*y[i]
         errors.append(total_error*(-1))
 
+    print("Weight Vector: ", w)
+
+    # Plot the Errors
     plt.plot(errors)
     plt.xlabel('Epoch')
     plt.ylabel('Total Error')
+    plt.show()
 
     return w
 
 
-w = perceptron_sgd_plot(X, y)
-
-print(w)
+perceptron_sgd_plot(X, y)
